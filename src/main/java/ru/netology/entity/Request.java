d992b80fae6c2a97cdf4e0c5c9219d0645483bb6
KoshelevVA <koshelevva96@gmail.com>
2025-05-05 18:08:30 +0400

    done web-1.2

--- src/main/java/ru/netology/entity/Request.java
+++ src/main/java/ru/netology/entity/Request.java
@@ -1,15 +1,32 @@
 package ru.netology.entity;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public class Request {
     private final String method;
-    private final String endpoint;
-    private final String versionHttp;
+    private final String path;
+    private final String httpVersion;
     private final String body;
+    private final Map<String, List<String>> queryParams;
 
-    public Request(String method, String endpoint, String versionHttp, String body) {
+    public Request(String method, String path, String httpVersion, String body, Map<String, List<String>> queryParams) {
         this.method = method;
-        this.endpoint = endpoint;
-        this.versionHttp = versionHttp;
+        this.path = path;
+        this.httpVersion = httpVersion;
         this.body = body;
+        this.queryParams = queryParams;
+    }
+
+    public Optional<String> getQueryParam(String name) {
+        return Optional.ofNullable(queryParams.get(name))
+                .filter(list -> !list.isEmpty())
+                .map(List::getFirst);
+    }
+
+    public List<String> getQueryParams(String name) {
+        return queryParams.getOrDefault(name, Collections.emptyList());
     }
 }
